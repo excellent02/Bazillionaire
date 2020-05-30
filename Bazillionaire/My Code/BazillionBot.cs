d0e849698ff829ea35d147075db96ac2d63ea8a7
Joe Mayo <joemayomax@gmail.com>
2020-05-29 23:00:37 -0400

    Still testing

--- Bazillionaire/My Code/BazillionBot.cs
+++ Bazillionaire/My Code/BazillionBot.cs
@@ -16,7 +16,7 @@ using System.Threading;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text.RegularExpressions;
 using System.Security.Cryptography.X509Certificates;
-
+//Test 
 namespace bazillionaire
 {
     public struct ConfigJson
